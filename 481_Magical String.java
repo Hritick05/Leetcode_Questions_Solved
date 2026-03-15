be67260a083fa40d87f58b1679d4f4f22449bd02
Hritick05 <150815002+Hritick05@users.noreply.github.com>
2026-03-16 01:52:44 +0530

    Add files via upload

---
+++ 481_Magical String.java
@@ -0,0 +1,35 @@
+public int magicalString(int n) {
+
+    if (n <= 0) return 0;
+    if (n <= 3) return 1;
+
+    int[] s = new int[n + 2];
+
+    s[0] = 1;
+    s[1] = 2;
+    s[2] = 2;
+
+    int head = 2;
+    int tail = 3;
+    int num = 1;
+    int count = 1;
+
+    while (tail < n) {
+
+        int times = s[head];
+
+        for (int i = 0; i < times; i++) {
+            s[tail] = num;
+
+            if (num == 1 && tail < n)
+                count++;
+
+            tail++;
+        }
+
+        num = (num == 1) ? 2 : 1;
+        head++;
+    }
+
+    return count;
+}
