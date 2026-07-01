37513f2ade748356524746d6535346786b64188d
Hritick Vishwas <150815002+Hritick05@users.noreply.github.com>
2026-07-01 23:15:25 +0530

    Add files via upload

---
+++ 405. Convert a Number to Hexadecimal.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+
+        char[] hex = {
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+        };
+
+        StringBuilder ans = new StringBuilder();
+
+        while (num != 0) {
+            ans.append(hex[num & 15]); // last 4 bits
+            num >>>= 4;                // unsigned right shift
+        }
+
+        return ans.reverse().toString();
+    }
+}
